10a11e464da2882e51b9c8f0d15fedf028e4531c
BFam <longlivelado@gmail.com>
2025-08-30 14:52:30 -0500

    added Notification

---
+++ Models/Entities/Notification.cs
@@ -0,0 +1,12 @@
+﻿namespace PFMApp.Models.Entities
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string NotificationType { get; set; }
+        public string NotificationContent { get; set; }
+        public DateTime NotificationDate { get; set; }
+        
+    }
+}
--- obj/Debug/net9.0/PFMApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/PFMApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PFMApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77e3966938ed59ae5b7b5cfe2abe22fe270afb98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f97ef87b6159c0e7a9e622d18ff702c73687bf4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PFMApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PFMApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
