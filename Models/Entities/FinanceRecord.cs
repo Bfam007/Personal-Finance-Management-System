a9475ac452b7908609aac105f8596c1665e14a79
BFam <longlivelado@gmail.com>
2025-08-30 13:05:57 -0500

    added Finance Record

---
+++ Models/Entities/FinanceRecord.cs
@@ -0,0 +1,14 @@
+﻿namespace PFMApp.Models.Entities
+{
+    public class FinanceRecord
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string FinanceRecordType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Note { get; set; }
+        public string Category { get; set; }
+
+    }
+}
